8078510d40dde5c615bde052185534cc64c1b3f1
Melvin Vice <you@example.com>
2023-12-14 14:58:16 +0700

    perubahan header

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -13,6 +13,16 @@
     <!-- Styles -->
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Aldrich&display=swap');
+
+        @keyframes slide {
+            0% {
+                transform: translateX(-200%);
+            }
+
+            100% {
+                transform: translateX(1000%);
+            }
+        }
     </style>
 
 
@@ -20,10 +30,10 @@
 
 <body class="font-aldrich">
     {{-- navbar --}}
-    <div class="w-full h-16 pl-5 grid content-center bg-green-900 text-white">
+    <div class="w-full h-16 pl-5 grid content-center bg-d-green text-white">
         <div class="flex">
             <div class="mr-5 flex-auto">
-               <img src="{{ asset('asset/img/logo_smk.png') }}" alt="" width="40px"> 
+                <img src="{{ asset('asset/img/logo_smk.png') }}" alt="" width="40px">
             </div>
             <div class="flex-auto my-auto">
                 Beranda
@@ -40,11 +50,40 @@
             <div class="flex-auto my-auto">
                 Kontak
             </div>
-            <div class="flex-auto my-auto pl-5 pt-2 pb-2 rounded-l-lg bg-white text-green-900">
+            <div class="flex-auto my-auto pl-5 pt-2 pb-2 rounded-l-lg bg-white text-d-green">
                 <div class="my-auto">
                     Profil
                 </div>
             </div>
+        </div>
+    </div>
+
+    {{-- header --}}
+    <div class="m-8 overflow-hidden">
+        <div class="w-100%  bg-l-sky-blue">
+            {{-- logo --}}
+            <div class="flex uppercase absolute">
+                <div class="m-5">
+                    <img src="{{ asset('asset/img/logo_smk.png') }}" alt="" width="120">
+                </div>
+                <div class="my-auto text-lg">
+                    Penerimaan Peserta Didik Baru <br>
+                    SMK Ma’arif NU Doro
+                </div>
+                
+            </div>
+
+            <div class="awan">
+                <img src="{{ asset('asset/img/awan.png') }}" alt=""
+                    style="animation: slide 20s linear infinite;">
+                <img src="{{ asset('asset/img/awan.png') }}" alt=""
+                    style="animation: slide 40s linear infinite;">
+                <img src="{{ asset('asset/img/awan.png') }}" alt=""
+                    style="animation: slide 28s linear infinite;">
+                <img src="{{ asset('asset/img/awan.png') }}" alt=""
+                    style="animation: slide 16s linear infinite;">
+            </div>
+
         </div>
     </div>
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,16 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
+    colors: {
+      'd-green': "#149200",
+      'white': "#FFF",
+      'l-sky-blue' :"#C1EBF9",
+      'blue': "#3498DB"
+    },
     fontFamily: {
       'aldrich': ['Aldrich'],
     },
+
     extend: {},
   },
   plugins: [],
